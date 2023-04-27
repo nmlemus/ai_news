692ec17058b519c4c8e3f39895c8f2257b344281
admin <admin@admins-MacBook-Pro.local>
2023-04-27 11:28:48 -0500

    Update views and add support button

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,26 +1,28 @@
 import 'package:get/get.dart';
 
-import '../modules/feed/bindings/feed_binding.dart';
-import '../modules/feed/views/feed_view.dart';
-import '../modules/feeds/bindings/feeds_binding.dart';
-import '../modules/feeds/views/feeds_view.dart';
+import '../modules/events/bindings/events_binding.dart';
+import '../modules/events/views/events_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/intro/bindings/intro_binding.dart';
 import '../modules/intro/views/intro_view.dart';
-import '../modules/landing/bindings/landing_binding.dart';
-import '../modules/landing/views/landing_view.dart';
+import '../modules/newsfeed/bindings/newsfeed_binding.dart';
+import '../modules/newsfeed/views/newsfeed_view.dart';
+import '../modules/notifications/bindings/notifications_binding.dart';
+import '../modules/notifications/views/notifications_view.dart';
 import '../modules/profile/bindings/profile_binding.dart';
 import '../modules/profile/views/profile_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
+import '../modules/timeline/bindings/timeline_binding.dart';
+import '../modules/timeline/views/timeline_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
@@ -29,24 +31,9 @@ class AppPages {
       binding: HomeBinding(),
     ),
     GetPage(
-      name: _Paths.SPLASH,
-      page: () => const SplashView(),
-      binding: SplashBinding(),
-    ),
-    GetPage(
-      name: _Paths.LANDING,
-      page: () => const LandingView(),
-      binding: LandingBinding(),
-    ),
-    GetPage(
-      name: _Paths.FEEDS,
-      page: () => const FeedsView(),
-      binding: FeedsBinding(),
-    ),
-    GetPage(
-      name: _Paths.FEED,
-      page: () => const FeedView(),
-      binding: FeedBinding(),
+      name: _Paths.INTRO,
+      page: () => const IntroView(),
+      binding: IntroBinding(),
     ),
     GetPage(
       name: _Paths.PROFILE,
@@ -54,9 +41,29 @@ class AppPages {
       binding: ProfileBinding(),
     ),
     GetPage(
-      name: _Paths.INTRO,
-      page: () => const IntroView(),
-      binding: IntroBinding(),
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.NEWSFEED,
+      page: () => const NewsfeedView(),
+      binding: NewsfeedBinding(),
+    ),
+    GetPage(
+      name: _Paths.TIMELINE,
+      page: () => const TimelineView(),
+      binding: TimelineBinding(),
+    ),
+    GetPage(
+      name: _Paths.EVENTS,
+      page: () => const EventsView(),
+      binding: EventsBinding(),
+    ),
+    GetPage(
+      name: _Paths.NOTIFICATIONS,
+      page: () => const NotificationsView(),
+      binding: NotificationsBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,21 +4,23 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
-  static const SPLASH = _Paths.SPLASH;
-  static const LANDING = _Paths.LANDING;
-  static const FEEDS = _Paths.FEEDS;
-  static const FEED = _Paths.FEED;
-  static const PROFILE = _Paths.PROFILE;
   static const INTRO = _Paths.INTRO;
+  static const PROFILE = _Paths.PROFILE;
+  static const SPLASH = _Paths.SPLASH;
+  static const NEWSFEED = _Paths.NEWSFEED;
+  static const TIMELINE = _Paths.TIMELINE;
+  static const EVENTS = _Paths.EVENTS;
+  static const NOTIFICATIONS = _Paths.NOTIFICATIONS;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
-  static const SPLASH = '/splash';
-  static const LANDING = '/landing';
-  static const FEEDS = '/feeds';
-  static const FEED = '/feed';
-  static const PROFILE = '/profile';
   static const INTRO = '/intro';
+  static const PROFILE = '/profile';
+  static const SPLASH = '/splash';
+  static const NEWSFEED = '/newsfeed';
+  static const TIMELINE = '/timeline';
+  static const EVENTS = '/events';
+  static const NOTIFICATIONS = '/notifications';
 }
